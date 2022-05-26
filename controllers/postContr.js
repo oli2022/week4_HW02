a8436194de9d81ca320f058d41012e82c90294ed
Oli <oli0970717429@gmail.com>
2022-05-26 19:37:33 +0800

    feat:新增錯誤訊息

--- controllers/postContr.js
+++ controllers/postContr.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const { errorHandle, deleteError, deleteAllError, nullError } = require('../servers/errorHandle');
+
 // 引用 postModel.js 檔案
 const postModel = require('../Models/postModel');
 
@@ -14,33 +16,53 @@ const postsController = {
         res.status(200).json(post);
     },
     createPost: async (req, res) => {
-        const newPost = await postModel.create(req.body);
-        res.status(200).json({
-            post: newPost,
-            status: ' 單筆資料新增成功 ',
-        });
+        try {
+            const newPost = await postModel.create(req.body);
+            res.status(200).json({
+                post: newPost,
+                status: ' 單筆資料新增成功 ',
+            });
+        } catch (error) {
+            nullError(res, error);
+        }
     },
     deleteAll: async (req, res) => {
-        await postModel.deleteMany({});
-        res.status(200).json({
-            status: ' 刪除全部資料成功 ',
-        });
+        try {
+            if (req.originalUrl === '/posts') {
+                await postModel.deleteMany({});
+                res.status(200).json({
+                    status: '刪除全部資料成功',
+                });
+            } else {
+                deleteAllError(res, error);
+            }
+        } catch (error) {
+            deleteAllError(res, error);
+        }
     },
     deleteOne: async (req, res) => {
-        const id = req.params.id;
-        await postModel.findByIdAndDelete(id);
-        res.status(200).json({
-            status: ' 單筆資料刪除成功 ',
-        });
+        try {
+            const id = req.params.id;
+            await postModel.findByIdAndDelete(id);
+            res.status(200).json({
+                status: ' 單筆資料刪除成功 ',
+            });
+        } catch (error) {
+            deleteError(res, error);
+        }
     },
     updatePost: async (req, res) => {
-        const id = req.params.id;
-        const data = req.body;
-        await postModel.findByIdAndUpdate(id, data);
-        res.status(200).json({
-            status: ' 單筆資料更新成功 ',
-            data,
-        });
+        try {
+            const id = req.params.id;
+            const data = req.body;
+            await postModel.findByIdAndUpdate(id, data);
+            res.status(200).json({
+                status: ' 單筆資料更新成功 ',
+                data,
+            });
+        } catch (error) {
+            deleteError(res, error);
+        }
     },
 };
 
